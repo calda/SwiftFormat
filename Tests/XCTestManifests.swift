0807c46756d23ad8fe59b177741cc082934b917e
Cal Stephens <cal@calstephens.tech>
2020-09-20 10:52:42 -0700

    Add extensionacl option to place the ACL keyword on the extension or on the body declarations

--- Tests/XCTestManifests.swift
+++ Tests/XCTestManifests.swift
@@ -1901,6 +1901,8 @@ extension RulesTests {
         ("testUnusedUntypedClosureArguments", testUnusedUntypedClosureArguments),
         ("testUnwrapFuncAttribute", testUnwrapFuncAttribute),
         ("testUnwrapTypeAttribute", testUnwrapTypeAttribute),
+        ("testUpdatedVisibilityOfExtension", testUpdatedVisibilityOfExtension),
+        ("testUpdatedVisibilityOfExtensionWithDeclarationsInConditionalCompilation", testUpdatedVisibilityOfExtensionWithDeclarationsInConditionalCompilation),
         ("testUpdatesMalformedMarks", testUpdatesMalformedMarks),
         ("testUpdatesVisibilityOfExtensionInConditionalCompilationBlock", testUpdatesVisibilityOfExtensionInConditionalCompilationBlock),
         ("testUpdatesVisibilityOfExtensionMembers", testUpdatesVisibilityOfExtensionMembers),
