57398ca402d8fffd5c4745e4160aed23ee24ba5a
Cal Stephens <cal.stephens@airbnb.com>
2024-06-10 10:31:25 -0700

    Add CI job to upload artifact to release

--- Scripts/spm-artifact-bundle.sh
+++ Scripts/spm-artifact-bundle.sh
@@ -4,9 +4,16 @@ set -e
 
 ARTIFACT_BUNDLE=swiftformat.artifactbundle
 INFO_TEMPLATE=Scripts/spm-artifact-bundle-info.template
-VERSION=$(./Scripts/get-version.sh)
 MAC_BINARY_OUTPUT_DIR=$ARTIFACT_BUNDLE/swiftformat-$VERSION-macos/bin
 LINUX_BINARY_OUTPUT_DIR=$ARTIFACT_BUNDLE/swiftformat-$VERSION-linux-gnu/bin
+
+# By default, parses the current version from `Sources/SwiftFormat.swift`.
+# Can be overridden by passing in custom version number as argument, e.g.
+# `./Scripts/spm-artifact-bundle.sh VERSION_NUMBER`.
+VERSION=${1:-$(./Scripts/get-version.sh)}
+
+rm -rf swiftformat.artifactbundle
+rm -rf swiftformat.artifactbundle.zip
 
 mkdir $ARTIFACT_BUNDLE
 
@@ -18,11 +25,11 @@ sed 's/__VERSION__/'"${VERSION}"'/g' $INFO_TEMPLATE > "${ARTIFACT_BUNDLE}/info.j
 
 # Copy macOS SwiftFormat binary into bundle
 mkdir -p $MAC_BINARY_OUTPUT_DIR
-cp CommandLineTool/swiftformat $MAC_BINARY_OUTPUT_DIR
+cp ${2:-CommandLineTool/swiftformat} $MAC_BINARY_OUTPUT_DIR
 
 # Copy Linux SwiftFormat binary into bundle
 mkdir -p $LINUX_BINARY_OUTPUT_DIR
-cp CommandLineTool/swiftformat_linux $LINUX_BINARY_OUTPUT_DIR
+cp ${3:-CommandLineTool/swiftformat_linux} $LINUX_BINARY_OUTPUT_DIR
 
 # Create ZIP
 zip -yr - $ARTIFACT_BUNDLE > "${ARTIFACT_BUNDLE}.zip"
